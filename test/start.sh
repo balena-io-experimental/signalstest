9eab428523f297ab0d06867319ea8a516aab5f40
Gergely Imreh <imrehg@gmail.com>
2018-07-26 14:16:32 +0100

    bash test more signals

--- test/start.sh
+++ test/start.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-trap 'increment' 2
+trap 'increment' 2 3 6 15
 
 increment()
 {
-  echo "Caught SIGINT ..."
+  echo "Caught SIGNAL ..."
   X=`expr ${X} + 500`
   if [ "${X}" -gt "1000" ]
   then
