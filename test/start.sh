719d2cadf84bb54ff2a1c39a32e1a9148376e72e
Gergely Imreh <imrehg@gmail.com>
2018-07-26 15:25:37 +0100

    debian test

---
+++ test/start.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+trap 'increment' 2 3 6 15
+
+increment()
+{
+  echo "Caught SIGNAL ..."
+  X=`expr ${X} + 500`
+  if [ "${X}" -gt "1000" ]
+  then
+    echo "Okay, I'll quit ..."
+    exit 1
+  fi
+}
+
+### main script
+X=0
+while :
+do
+  echo "X=$X"
+  X=`expr ${X} + 1`
+  sleep 1
+done
